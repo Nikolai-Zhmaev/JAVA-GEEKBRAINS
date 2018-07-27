a8d8f3de28c15399069718ef0d50e3188ab4a718
Николай Жмаев <nik.zhmaev@gmail.com>
2018-07-27 19:12:04 +0600

    Домашняя работа - 5

--- Item.java
+++ Item.java
@@ -1,51 +1,13 @@
 package ru.geekbrains.MyPractice;
 
-// класс объекта
-
-import java.util.Objects;
-
 public class Item {
 
-    private final int value; // поле для хранения значения (final - так как изменяться не будет);
-    private Item nextItem; // поле для зранения ссылки на следующий элемент
+    int weight, price; // создадим поля для хранения веса и стоимости предмета.
+    String name; // поле для хранения названия предмета.
 
-    public Item(int value) {
-
-        this.value = value;
-    }
-
-// создаем геттеры и сеттеры для допуступа к приватным полям
-
-    public int getValue() {
-        return value;
-    }
-
-    public void setNextItem(Item nextItem) {
-
-        this.nextItem = nextItem;
-    }
-
-    public Item getNextItem() {
-
-        return nextItem;
-    }
-
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Item item = (Item) o;
-        return value == item.value &&
-                Objects.equals(nextItem, item.nextItem);
-    }
-
-
-    @Override
-    public String toString() {
-        return "Item{" +
-                "value=" + value +
-                ", nextItem=" + nextItem +
-                '}';
+    public Item(int weight, int price, String name) {
+        this.weight = weight;
+        this.price = price;
+        this.name = name;
     }
 }
